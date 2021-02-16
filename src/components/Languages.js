50feae989c346ee9ad6c9e093fb9c0eaea9c4ea6
品君林 <jinglelin82@gmail.com>
2021-02-16 15:25:47 +0900

    加入語言切換

---
+++ src/components/Languages.js
@@ -0,0 +1,18 @@
+import { useTranslation } from "react-i18next";
+
+const Languages = () => {
+  const { t, i18n } = useTranslation();
+
+   const changeLanguage = (lng) => {
+      i18n.changeLanguage(lng);
+   };
+  return (
+    <ul className="nav d-flex">
+      <li><button onClick={() => changeLanguage('zh-TW')} className="btn btn-outline">中文</button></li>
+      <li><button onClick={() => changeLanguage('en')} className="btn btn-outline">English</button></li>
+      <li><button onClick={() => changeLanguage('kr')} className="btn btn-outline">한국어</button></li>
+    </ul>
+  );
+}
+
+export default Languages;
