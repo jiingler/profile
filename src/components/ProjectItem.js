311b1f8c1c5a010db4efeed17d8118934474a1fd
品君林 <jinglelin82@gmail.com>
2021-02-25 16:33:05 +0900

    增加go按鈕

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,13 +1,16 @@
+import goImg from '../assets/images/go.svg';
+
 const ProjectItem = ({imgUrl, projectTitle, projectDescription, projectTags }) => {
   return (
     <a href="https://jiingler.github.io/hipster_korea/" target="_blank">
-      <figure className="project effect px-0">
+      <figure className="project px-0">
         <img src={imgUrl} alt="project" className="project-img"/>
         <figcaption className="project-info">
           <h5 className="project-title fw-bolder">{projectTitle}</h5>
           <p className="project-description">{projectDescription}</p>
           <p className="project-tags">{projectTags}</p>
         </figcaption>
+        <img className="go-img" src={goImg} alt="go icon"/>
       </figure>
     </a>
   );
